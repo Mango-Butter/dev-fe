66c81f9e16749bc4bef244bb2aaa51d48acad141
Yoon seokchan <ysc0731@ajou.ac.kr>
2025-03-28 15:05:32 +0900

    init: pwa 프로젝트 초기설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true, // 개발 환경에서도 서비스 워커와 매니페스트 생성
+      },
+      // service worker
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"], // service worker 캐싱
+      },
+      // pwa
+      manifest: {
+        start_url: "/",
+        name: "MangoBoss",
+        short_name: "MangoBoss",
+        description: "MangoBoss : Part-time Worker Management WebApp",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
