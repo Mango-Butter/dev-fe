b46f5c740423ce8c7d4d68a455ce6f3415b90dc6
Yoon seokchan <ysc0731@ajou.ac.kr>
2025-04-02 22:33:37 +0900

    test: QR 스캔 컴포넌트 테스íPOC ¸코드 작성 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import QrScanner from "./components/QrScanner.tsx";
+
 export default function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-500">Mango-Butter</h1>
+      <QrScanner />
     </div>
   );
 }
