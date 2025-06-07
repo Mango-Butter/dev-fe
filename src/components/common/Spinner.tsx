28f45ebf2544dfd2ad8a4f3fe07e25e0eb391518
Yoon seokchan <ysc0731@ajou.ac.kr>
2025-06-07 23:37:58 +0900

    feat: 매장 중복등록 버튼 중복 클릭 방지 (#65)

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Spinner: React.FC = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="w-8 h-8 border-4 border-primary border-t-transparent rounded-full animate-spin" />
+      <div className="w-5 h-5 border-4 border-primary border-t-transparent rounded-full animate-spin" />
     </div>
   );
 };
