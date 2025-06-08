37543037820c60e888aa03297f05bb839af7353a
Yoon seokchan <ysc0731@ajou.ac.kr>
2025-06-08 15:49:44 +0900

    refactor: 주간 캘린더 월 표시 및 UI 개선 (#67)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.92vk4gtbmag"
+    "revision": "0.3lvg877pghg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
